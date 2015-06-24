fa8977cb322851e1a488bb47e5bd2b04201694d2
Josh Higgins <jshhggns@gmail.com>
2015-06-24 15:28:01 -0400

    Error

--- urlybird/api/serializer.py
+++ urlybird/api/serializer.py
@@ -2,6 +2,7 @@ __author__ = 'joshuahiggins'
 from rest_framework import serializers
 from bookmark.models import Bookmark
 from click.models import Click
+from hashids import Hashids
 
 
 class ClickSerializer(serializers.HyperlinkedModelSerializer):
@@ -20,3 +21,14 @@ class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
         model = Bookmark
         fields = ('id', 'user', 'long', 'description', 'title', 'url', 'short', 'created', 'number_clicks', 'edited',
                   'clicks')
+
+        # def create(self, validated_data):
+        #     hashids = Hashids(min_length = 4, salt="ArloBeaIdaKatie")
+        #     previous = Bookmark.objects.latest('id')
+        #     previousid = previous.id
+        #     if previous.id is None:
+        #         previousid = 0
+        #     bookmark = Bookmark.objects.create(**validated_data)
+        #     bookmark.short = hashids.encrypt(previousid + 1)
+        #     bookmark.save()
+        #     return bookmark
