c3de07f776c212586a0848a8f7b73f94a93dab5a
Josh Higgins <jshhggns@gmail.com>
2015-06-24 16:34:08 -0400

    Fixed Normal Mode

--- urlybird/api/serializer.py
+++ urlybird/api/serializer.py
@@ -16,19 +16,23 @@ class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     clicks = serializers.HyperlinkedIdentityField(view_name='click-list')
     #maybe?
+    short = serializers.CharField(read_only=True)
 #    url = serializers.HyperlinkedIdentityField(view_name='bookmark-detail')
+
+    def create(self, validated_data):
+            hashids = Hashids(min_length = 4, salt="ArloBeaIdaKatie")
+            previous = Bookmark.objects.latest('id')
+            previousid = previous.id
+            if previous.id is None:
+                previousid = 0
+            bookmark = Bookmark.objects.create(**validated_data)
+            bookmark.short = hashids.encrypt(previousid + 1)
+            bookmark.save()
+            return bookmark
+
     class Meta:
         model = Bookmark
         fields = ('id', 'user', 'long', 'description', 'title', 'url', 'short', 'created', 'number_clicks', 'edited',
                   'clicks')
 
-        # def create(self, validated_data):
-        #     hashids = Hashids(min_length = 4, salt="ArloBeaIdaKatie")
-        #     previous = Bookmark.objects.latest('id')
-        #     previousid = previous.id
-        #     if previous.id is None:
-        #         previousid = 0
-        #     bookmark = Bookmark.objects.create(**validated_data)
-        #     bookmark.short = hashids.encrypt(previousid + 1)
-        #     bookmark.save()
-        #     return bookmark
+
